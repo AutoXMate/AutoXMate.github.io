75a22a39c403f067f98421e9ff18433780e2d439
AutoXMate <rootiwari0@gmail.com>
2026-06-14 16:06:27 +0530

    Fix site URL: intro-shlok -> autoxmate

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://intro-shlok.github.io/AutoXMate",
+  site: "https://autoxmate.github.io/AutoXMate",
   base: "/AutoXMate/",
   output: "static",
   integrations: [mdx()],
